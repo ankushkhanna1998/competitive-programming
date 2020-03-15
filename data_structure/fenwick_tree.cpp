f19075e492939b80fa336a6a8cddbf9ffedc676a
Ankush Khanna <ankushkhanna1998@gmail.com>
2020-03-15 20:42:17 +0530

    adds fenwick tree code

---
+++ data_structure/fenwick_tree.cpp
@@ -0,0 +1,30 @@
+template <typename T>
+class fenwick_tree {
+public:
+
+    int n;
+
+    vector<T> fenw;
+
+    inline fenwick_tree(int _n) : n(_n) {
+        fenw.resize(n);
+    }
+
+    inline void add(int x, T delta) {
+        while (x < n) {
+            fenw[x] += delta;
+            x |= x + 1;
+        }
+    }
+
+    inline T sum(int x) {
+        T res = static_cast<T>(0L);
+        while (x >= 0) {
+            res += fenw[x];
+            x &= x + 1;
+            x--;
+        }
+        return res;
+    }
+
+};
