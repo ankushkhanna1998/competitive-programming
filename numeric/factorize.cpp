cea46aea1e6f2ce881d9272556c9f32a19865907
Ankush Khanna <ankushkhanna1998@gmail.com>
2020-04-01 05:35:51 +0530

    adds factorization code

---
+++ numeric/factorize.cpp
@@ -0,0 +1,53 @@
+template <typename T>
+inline vector<T> factorize(T n) {
+    vector<T> res;
+    while (!(n & 1)) {
+        n >>= 1;
+        res.push_back(2);
+    }
+    for (T i = 3; i * i <= n; i += 2) {
+        while (!(n % i)) {
+            n /= i;
+            res.push_back(i);
+        }
+    }
+    if (n > static_cast<T>(2)) {
+        res.push_back(n);
+    }
+    return res;
+}
+
+class factorizer {
+public:
+
+    const int MAXN;
+
+    vector<int> spf;
+
+    inline factorizer(int _n) : MAXN(_n), spf(_n + 1) {
+        iota(spf.begin(), spf.end(), 0);
+        for (int i = 4; i <= MAXN; i += 2) {
+            spf[i] = 2;
+        }
+        for (int i = 3; i * i <= MAXN; i++) {
+            if (spf[i] == i) {
+                for (int j = i * i; j <= MAXN; j += i) {
+                    if (spf[j] == j) {
+                        spf[j] = i;
+                    }
+                }
+            }
+        }
+    }
+
+    inline vector<int> factorize(int n) {
+        assert(n > 0 && n <= MAXN);
+        vector<int> res;
+        while (n > 1) {
+            res.push_back(spf[n]);
+            n /= spf[n];
+        }
+        return res;
+    }
+
+};
