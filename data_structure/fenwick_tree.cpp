433a9f23facfd53b1e1b843f6618378e324bc260
Ankush Khanna <ankushkhanna1998@gmail.com>
2020-08-11 10:43:59 +0530

    adds order statistics for fenwick tree

--- data_structure/fenwick_tree.cpp
+++ data_structure/fenwick_tree.cpp
@@ -35,4 +35,23 @@ public:
         return sum(r) - sum(l - 1);
     }
 
+    inline int order_of_key(const T val) const {
+        return (val == 0 ? 0 : (int) sum(val - 1));
+    }
+
+    inline T find_by_order(const int pos) const {
+        T res = -1;
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            if (sum(mid) >= pos + 1) {
+                res = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
+
 };
