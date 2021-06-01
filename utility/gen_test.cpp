8b251b10c8668d738204e18f26783c21c3d0c720
Ankush Khanna <ankushkhanna1998@gmail.com>
2021-06-01 16:34:56 +0530

    modifies gen_test: adds keyboard interrupt with tests_run and updates 64 bit RNG Engine

--- utility/gen_test.cpp
+++ utility/gen_test.cpp
@@ -1,23 +1,33 @@
 /**
  *     Author: ankushkhanna (Ankush Khanna)
- *    Created: Tuesday, 08 December 2019, 06:33:17 AM IST
+ *    Created: Tuesday, 01 June 2021, 03:46:54 PM IST
 **/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    char buf[64];
-    random_device rng;
-    while (true) {
+volatile uint64_t tests_run = 0;
 
+void signal_handler(int signal_code) {
+    cerr << endl << "Keyboard Interrupt!" << endl;
+    cerr << " Tests Run: " << tests_run << endl;
+    exit(signal_code);
+}
+
+int main() {
+    signal(SIGINT, signal_handler);
+
+    char buf[64];
+    mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
+
+    while (true) {
         ofstream fout("in.txt", ios_base::out);
         /* Generate a test case here... */
         fout.close();
 
         string a, b;
 
-        FILE *fp = popen("(./A < in.txt) 2> /tmp/stderr.txt", "r"); // Your Code
+        FILE *fp = popen("(./A < in.txt) 2> /tmp/stderr.txt", "r");  // Your Code
         if (fp == nullptr) {
             cerr << "File Problem at line " << __LINE__ << endl;
             exit(0);
@@ -27,7 +37,7 @@ int main() {
         }
         pclose(fp);
 
-        fp = popen("(./B < in.txt) 2> /tmp/stderr.txt", "r"); // Correct Code
+        fp = popen("(./B < in.txt) 2> /tmp/stderr.txt", "r");  // Correct Code
         if (fp == nullptr) {
             cerr << "File Problem at line " << __LINE__ << endl;
             exit(0);
@@ -38,6 +48,7 @@ int main() {
         pclose(fp);
 
         remove("/tmp/stderr.txt");
+        ++tests_run;
 
         if (a != b) {
             cout << "\n  Me: " << a;
@@ -46,10 +57,9 @@ int main() {
                 cerr << "Unexpected Error at line " << __LINE__ << endl;
                 exit(0);
             }
-            cout << '\n';
+            cout << endl;
             break;
         }
-
     }
     return 0;
 }
